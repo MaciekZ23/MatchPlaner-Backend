7e769c1907d351c07d7c322e6afa28d849f7e73d
MaciekZ23 <maciekz232002@o2.pl>
2025-10-21 21:41:34 +0200

    listen

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
     swaggerOptions: { persistAuthorization: true },
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
